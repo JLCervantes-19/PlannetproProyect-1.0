6bf7b4f1d97a4d7eea2a2ce5c6df0e0c5d15274c
macuser <macuser@localhost>
2024-05-18 19:57:25 -0500

    mesaje

--- target/generated-sources/annotations/com/mycompany/Controller/Evento_.java
+++ target/generated-sources/annotations/com/mycompany/Controller/Evento_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-05-16T12:04:50", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-05-18T14:20:07", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Evento.class)
 public class Evento_ { 
 
